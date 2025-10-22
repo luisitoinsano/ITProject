8808052222ab3f4601177cd67f7f0fe712aee506
luisitoinsano <luislotux0@gmail.com>
2025-10-22 08:29:38 -0600

    Creando cosas

---
+++ detector_emociones/venv/Scripts/Activate.ps1
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a0cde614269550977f1246db7eaf9ca5a552306dd067433c319c99d40aa10da8
+size 25697
---
+++ detector_emociones/venv/Scripts/activate.bat
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:439ec5cadafbaee18a6ed4da182c5d36b3c142e0939de93d327dc027db8f27ff
+size 998
